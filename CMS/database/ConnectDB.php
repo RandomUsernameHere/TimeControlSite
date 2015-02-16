3b7350c5188b1604410a7c02365b4d47bf20c435
Mitin <you@example.com>
2015-02-16 23:08:49 +0300

    Connect to database (without testing)

--- CMS/database/ConnectDB.php
+++ CMS/database/ConnectDB.php
@@ -4,16 +4,49 @@ namespace CMS\Database;
 
 
 class ConnectDB {
-	public static function MakeConnection(){
 
+    protected static $_instance;
+    protected $PDO;
+    protected $ErrorMessage;
+
+
+	public static function getInstance(){
+        if(!self::$_instance){
+            self::$_instance = new ConnectDB();
+        }
+        return self::$_instance;
 	}
 
-	protected function __construct(){
-
+	private function __construct(){
+        $this->SetPDO();
 	}
+
+    private function __clone(){}
 
 	protected function GetAccessString(){
 		require_once '/CMS/database/config.php';
 
+        return DATABASE_DRIVER.':'.
+        'host='.DATABASE_HOST.';'.
+        'dbname='.DATABASE_NAME.','.
+        DATABASE_USERNAME.','.
+        DATABASE_PASSWORD;
 	}
+
+    protected function SetPDO(){
+        try{
+            $this->PDO = new \PDO($this->GetAccessString());
+        }
+        catch(\PDOException $e){
+            $this->ErrorMessage = $e->getMessage();
+        }
+    }
+
+    public function getErrorMessage(){
+        return $this->ErrorMessage;
+    }
+
+    public function getPDO(){
+        return $this->PDO;
+    }
 }
